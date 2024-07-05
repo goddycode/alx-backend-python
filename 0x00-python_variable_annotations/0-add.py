456576bb450e48d1430cbd512a9fb2c57fbd1ae8
goddycode <odhisgoddy24@gmail.com>
2024-07-05 05:57:35 +0000

    Updating '0-add.py'

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+'''
+type-annotated function
+'''
 
 
 def add(a: float, b: float) -> float:
-   return a + b
+    '''function that add two float and return their output
+    '''
+    return (a + b)
